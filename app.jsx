fe3150f92115716d7258adea7138d64f30abb3dd
Winston Galas <BrianisWinston@yahoo.com>
2018-10-07 23:53:46 -0700

    Create logic for integrating react in html

---
+++ app.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      testing it out.
+    </div>
+  )
+}
+
+export default App;
--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from'react-dom';
-import 'App' from './app';
+import App from './app.jsx';
 
 document.addEventListener('DOMContentLoaded', () => {
-
   ReactDOM.render(<App/>, document.getElementById('root'));
 })
