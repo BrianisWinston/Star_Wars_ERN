b0352112759ed3cb72f44a90443336a1bfd1527a
Winston Galas <BrianisWinston@yahoo.com>
2018-10-10 15:37:16 -0700

    add changes to webpack

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
 import React from 'react';
 import ReactDOM from'react-dom';
 import App from '../app.jsx';
+import _ from 'lodash';
+
+function component() {
+  let element = document.createElement('div');
+
+  // Lodash, currently included via a script, is required for this line to work
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+
+  return element;
+}
+
+document.body.appendChild(component());
 
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(<App/>, document.getElementById('root'));
--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,9 @@
 const path = require('path');
 
 module.exports = {
-  entry: "./src/index.js",
+  entry: './src/index.js',
   output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname)
-  },
-  resolve: {
-    extensions: ['', '.js', '.jsx']
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
   }
 };
