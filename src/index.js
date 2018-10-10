6f2595b4c3ec06d546b068b0bd6353829335d694
Winston Galas <BrianisWinston@yahoo.com>
2018-10-10 15:28:35 -0700

    Separate files into src and dist folders

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from'react-dom';
-import App from './app.jsx';
+import App from '../app.jsx';
 
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(<App/>, document.getElementById('root'));
