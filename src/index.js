36f5c966fba18de36d4fb1dbf907ea54aba17492
Winston Galas <BrianisWinston@yahoo.com>
2018-10-11 21:44:30 -0700

    Try to fix webpack issues

--- src/index.js
+++ src/index.js
@@ -1,19 +1,27 @@
-import React from 'react';
-import ReactDOM from'react-dom';
-import App from '../app.jsx';
-import _ from 'lodash';
+// import React from 'react';
+// import ReactDOM from'react-dom';
+// import App from './app.jsx';
+// // import _ from 'lodash';
+// //
+// // function component() {
+// //   let element = document.createElement('div');
+// //
+// //   // Lodash, currently included via a script, is required for this line to work
+// //   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+// //
+// //   return element;
+// // }
+// //
+// // document.body.appendChild(component());
+//
+// document.addEventListener('DOMContentLoaded', () => {
+//   ReactDOM.render(<App/>, document.getElementById("root"));
+// })
+import React from "react";
+import ReactDOM from "react-dom";
 
-function component() {
-  let element = document.createElement('div');
+const Index = () => {
+  return <div>Hello React!</div>;
+};
 
-  // Lodash, currently included via a script, is required for this line to work
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
-  return element;
-}
-
-document.body.appendChild(component());
-
-document.addEventListener('DOMContentLoaded', () => {
-  ReactDOM.render(<App/>, document.getElementById('root'));
-})
+ReactDOM.render(<Index />, document.getElementById("index"));
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,21 @@
-const path = require('path');
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+const htmlPlugin = new HtmlWebPackPlugin({
+  template: "./src/index.html",
+  filename: "./index.html"
+});
 
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
-  }
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      }
+    ]
+  },
+  plugins: [htmlPlugin]
 };
